3cfe4e65a6938672b20fe44fb2152fbf0fb39529
Iryna Kutsenko <79517296+irynakutsenko@users.noreply.github.com>
2021-10-14 09:27:25 +0200

    NumPositivoNegativo

---
+++ js09.js
@@ -0,0 +1,12 @@
+let num;
+num = prompt('Ingrese un valor entero');
+num = parseInt(num);
+if (num == 0) {
+    document.write('El número ingresado es cero');
+} else {
+    if (num > 0) {
+        document.write('El número ingresado es positivo');
+    } else {
+        document.write('El número ingresado es negativo');
+    }
+}
